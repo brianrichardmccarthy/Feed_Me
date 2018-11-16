3315011b710b3f5a901b5625cf695c8032c3231e
Brian McCarthy <brianmccarthy@Brians-Mac.local>
2018-11-16 10:27:05 +0000

    Restarting assignment

---
+++ Feed Me/Feed Me/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  Feed Me
+//
+//  Created by Brian McCarthy on 16/11/2018.
+//  Copyright © 2018 Brian McCarthy. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        //setUpPhysics()
+        //etUpScenery()
+        //setUpPrize()
+        //setUpVines()
+        //setUpCrocodile()
+        //setUpAudio()
+    }
+    
+    //MARK: - Level setup
+    
+    fileprivate func setUpPhysics() { }
+    fileprivate func setUpScenery() { }
+    fileprivate func setUpPrize() { }
+    
+    //MARK: - Vine methods
+    
+    fileprivate func setUpVines() { }
+    
+    //MARK: - Croc methods
+    
+    fileprivate func setUpCrocodile() { }
+    fileprivate func animateCrocodile() { }
+    fileprivate func runNomNomAnimationWithDelay(_ delay: TimeInterval) { }
+    
+    //MARK: - Touch handling
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { }
+    fileprivate func showMoveParticles(touchPosition: CGPoint) { }
+    
+    //MARK: - Game logic
+    
+    override func update(_ currentTime: TimeInterval) { }
+    func didBegin(_ contact: SKPhysicsContact) { }
+    fileprivate func checkIfVineCutWithBody(_ body: SKPhysicsBody) { }
+    fileprivate func switchToNewGameWithTransition(_ transition: SKTransition) { }
+    
+    //MARK: - Audio
+    
+    fileprivate func setUpAudio() { }
+}
